1e9ad4c40fa27cea3aeb728b3c7251dafdbf9737
Vanessa Alves Pereira Osaka <vanessa@osaka.local>
2020-11-16 21:27:27 -0300

    Alteracao para funcionar desafio 1

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 
-// const { v4: uuid, validate: isUuid } = require('uuid');
+const { v4: uuid, validate: isUuid } = require('uuid');
 
 const app = express();
 
@@ -10,24 +10,83 @@ app.use(cors());
 
 const repositories = [];
 
+
 app.get("/repositories", (request, response) => {
-  // TODO
+  return response.json(repositories);
+
 });
 
 app.post("/repositories", (request, response) => {
-  // TODO
+  const { title, url, techs } = request.body;
+
+  const repository = {
+    id: uuid(),
+    title,
+    url,
+    techs,
+    likes: 0,
+  }
+
+  repositories.push(repository);
+
+  return response.json(repository)
 });
 
 app.put("/repositories/:id", (request, response) => {
-  // TODO
+  const { id } = request.params;
+  const { title, url, techs } = request.body;
+
+  const repoIndex = repositories.findIndex(repository => repository.id === id);
+
+  if (repoIndex < 0) {
+    return response.status(400).json({ error: 'Project not found' })
+  }
+
+  const repository = {
+    ...repositories[repoIndex],
+    id,
+    title,
+    url,
+    techs,
+  }
+
+  repositories[repoIndex] = repository;
+  return response.json(repository);
+
 });
 
 app.delete("/repositories/:id", (request, response) => {
-  // TODO
+  const { id } = request.params;
+
+  const repoIndex = repositories.findIndex(repository => repository.id === id);
+
+  if (repoIndex < 0) {
+    return response.status(400).json({ error: 'Project not found' })
+  }
+
+  repositories.splice(repoIndex, 1);
+
+  return response.status(204).send();
 });
 
 app.post("/repositories/:id/like", (request, response) => {
-  // TODO
+  const { id } = request.params;
+
+  const repoIndex = repositories.findIndex(repository => repository.id === id);
+
+  if (repoIndex < 0) {
+    return response.status(400).json({ error: 'Project not found' })
+  }
+
+  const repository = {
+    ...repositories[repoIndex],
+    likes: repositories[repoIndex].likes + 1
+  }
+
+  repositories[repoIndex] = repository;
+
+  return response.json(repository);
 });
+
 
 module.exports = app;
